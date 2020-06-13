8aced53994228a4ac6c357eac72827b43dfa212a
Jefferson Candido <jefferson.ufu@gmail.com>
2020-06-13 14:44:23 -0300

    OpenID Connect proof-of-concept

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Simple React App to make a proof-of-concenpt about OpenID Connect.
         </p>
         <a
           className="App-link"
